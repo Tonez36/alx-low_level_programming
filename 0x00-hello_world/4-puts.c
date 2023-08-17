6fb99baf58857bed2df8cb6ed29dbff90cb49660
Tonez36 <anthonychibuzo36@gmail.com>
2023-08-16 17:25:21 -0700

    C hello world updated

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - C program to print using puts function
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
