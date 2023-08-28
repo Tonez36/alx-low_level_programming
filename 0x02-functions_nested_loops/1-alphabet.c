5f9aaf4ee8bde0439c19c69d306c6e6bec8db55a
Tonez36 <anthonychibuzo36@gmail.com>
2023-08-28 14:40:37 -0700

    alphabet updat

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * main - make alphabet
+ * print_alphabet - make alphabet
  *
  * Return: Always 0
  */
 void print_alphabet(void)
 {
 	char az;
+
 	for (az = 'a'; az <= 'z'; az++)
 	{
 		_putchar(az);
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
